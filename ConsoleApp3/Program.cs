d30b887db8b40a0103859a38b6d4370cb6cf09f2
STUDENT <STUDENT@K-405-2>
2024-01-26 09:04:37 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a2 = Console.ReadLine();
+            int aa2 = int.Parse(a2);
+            if (aa2<18)
+            {
+                Console.WriteLine("ты не годен, иди молоко пей");
+            }
+            else
+            {
+                Console.WriteLine("родина ждет");
+            }
+        }
+    }
+}
